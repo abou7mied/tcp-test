ae463f5c0c83066fe76ed552c9a3bb2fe6ba1b9c
Ahmed Sabry <abou7mied.soft@gmail.com>
2017-02-02 00:44:44 +0200

    change to es5

--- server.js
+++ server.js
@@ -2,16 +2,14 @@
  * Created by abou7mied on 2/2/17.
  */
 
-"use strict";
-
-let debug = require("debug")("server");
-let http = require('http');
-let server_port = process.env.OPENSHIFT_NODEJS_PORT || 2222;
-let server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
+var debug = require("debug")("server");
+var http = require('http');
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 2222;
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1';
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/plain'});
   res.end("Hello!");
-}).listen(server_port, server_ip_address, () => {
+}).listen(server_port, server_ip_address, function() {
   debug("Server started!");
 });
